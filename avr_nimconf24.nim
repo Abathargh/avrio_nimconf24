fd78183b576f84bc0cdceefbdf43ff4280dcfcf6
mar <gim.marcello@gmail.com>
2024-10-06 17:45:19 +0200

    added: support and limitations, avrman, blink example.

--- avr_nimconf24.nim
+++ avr_nimconf24.nim
@@ -6,6 +6,7 @@ import nimiSlides
 
 nbinit(theme=revealTheme)
 footer("© G. Marcello, 2024, all rights reserved.")
+nb.useLatex()
 
 # modified code blocks
 
@@ -146,10 +147,10 @@ slide:
   slide:
     nbText: "Defining Interrupt Service Routines"
     animateCode(3, 4, 7):
-      const tim1Led = 5
+      const tim0Led = 5
 
-      proc timer1CompaIsr() {.isr(Timer1CompAVect).} =
-        portB.togglePin(tim1Led)
+      proc timer1CompaIsr() {.isr(Timer0CompAVect).} =
+        portB.togglePin(tim0Led)
 
       proc main =
         sei()
@@ -203,6 +204,8 @@ slide:
       let 
         header {.section(".metadata").} = fill(headerSize, 0'u8)
   slide:
+    nbImage("section.png")
+  slide:
     nbText: "Perpiheral support (partial, experimental)"
     unorderedList:
       listItem: nbText: "Not part of the original project idea, nice to have"
@@ -212,7 +215,7 @@ slide:
     nbText: "Perpiheral support: usart module"
     animateCode(3, 4, 5, 7..9):
       import avr_io
-      proc loop = 
+      proc usartMain = 
         const baud = baudRate(9600'u32) 
         usart0.initUart(baud, {}, {txen, rxen}, {ucsz1, ucsz0})
         var buf: array[100, cchar]
@@ -236,5 +239,108 @@ slide:
         timer0.setTimerFlag({TimCtlBFlag.cs0})
         timer0.ocra[] = ocrVal.uint8
 
+
+slide:
+  nbText: "## Support & limitations"
+  slide:
+    unorderedList:
+      listItem: nbText: "Supports only a subset of AVR chips (for now)"
+      unorderedList:
+        listItem: nbText: "Mainly ATMega ones I can test myself"
+        listItem: nbText: "The process to add a MCU is semi-automated"
+      listItem: nbText: "Works with avr-gcc only"
+      listItem: nbText: "Requires a bit of project configuration"
+  slide:
+    nbText: "panicoverride.nim"
+    animateCode(1, 3, 4..8):
+      proc exit(code: int) {.importc, header: "<stdlib.h>", cdecl.}
+      {.push stack_trace: off, profiler:off.}
+      proc rawoutput(s: string) = discard
+      proc panic(s: string) =
+        rawoutput(s)
+        while true:
+          discard
+        exit(1)
+      {.pop.}
+    nbRawHtml: """
+      <small>
+      Based on 
+      https://github.com/nim-lang/Nim/blob/devel/tests/avr/panicoverride.nim
+      </small>"""
+  slide:
+    nbText: "avr-specific configs"
+    nbCodeSkip:
+      switch("os", "standalone")
+      switch("cpu", "avr")
+      switch("gc", "none")
+      switch("define", "USING_ATMEGA328P")
+      switch("passC", "-mmcu=atmega328p -DF_CPU=16000000")
+      switch("passL", "-mmcu=atmega328p -DF_CPU=16000000")
+      switch("avr.standalone.gcc.options.linker", "-static")
+      switch("avr.standalone.gcc.exe", "avr-gcc")
+      switch("avr.standalone.gcc.linkerexe", "avr-gcc")
+
+slide:
+  nbText: "## avrman: *avr man*ager"
+  slide:
+    unorderedList:
+      listItem: nbText: "a tool for managing AVR projects"
+      listItem: nbText: "can initialize C and nim projects"
+      listItem: nbText: "avr_io is used as the base dependency for nim ones"
+      listItem: nbText: "supports make and CMake for C ones"
+  slide:
+    nbText: "Initialize an Arduino Uno based project"
+    nbCodeSkip: 
+      avrman init -m:atmega328p -f:16000000   \
+                  -p:"arduino -b 115200 -P:/dev/ttyACM0" uno
+    nbText: "Initialize an Arduino Uno based C project with Makefile"
+    nbCodeSkip: avrman init -m:atmega328p -f:16000000 \
+      -p:"arduino -b 115200 -P:/dev/ttyACM0" --cproject uno_c
+    nbText: "Initialize an Arduino Uno based C project with CMake"
+    nbCodeSkip: avrman init -m:atmega328p -f:16000000 \
+      -p:"arduino -b 115200 -P:/dev/ttyACM0" \ 
+      --cproject --cmake uno_cmake
+
+slide:
+  nbText: "## example: a simple blink application"
+  slide: nbText: "Let's blink the Arduino Uno in-builtin LED at a frequency of ~1Hz"
+  slide: nbImage("blink_init.png")
+  slide: nbText: "And now... a little bit of math"
+  slide:
+    nbText: """$f_{MCU} = 16 MHz$"""
+    nbText: """$f_{timer} = \frac{f_{MCU}}{prescaler factor} = \frac{16 MHz}{1024} = 15.625 KHz$"""
+  slide:
+    nbText: """$T_{timer} = \frac{1}{f_{timer}} = \frac{1}{15.625 KHz} = 64 μs$"""
+    nbText: """$T_{interrupt} = T_{timer} \cdot OCRA$""" 
+    nbText: """$= 64 μs \cdot 10000 = 0,64 s$"""
+  slide:
+    animateCode(2..3, 6..7, 8..9, 11..12):
+      import avr_io
+      const 
+        builtinLed = 5'u8
+      
+      proc initTimer0() =
+        OCR1AH[]  = (10000 shr 8).uint8
+        OCR1AL[]  = (10000 and 0xff).uint8
+        timer1.setTimerFlag({TimCtlB16Flag.cs0, cs2, wgm2})
+        timer1.setTimerFlag({Timsk16Flag.ociea})
+      
+      proc timer0_compa_isr() {.isr(Timer1CompAVect).} =
+        portB.togglePin(builtinLed)
+
+  slide:
+    nbCodeInBlock:
+      proc loop = 
+        # PORTB[5] is the Arduino Uno builtin led 
+        portB.asOutputPin(builtinLed)
+        initTimer0()
+        sei()
+        while true:
+          discard
+  slide: nbVideo("arduino_blink.mp4")
+
+slide:
+  nbText: "## example: building a synth"
+  
 
 nbSave()
